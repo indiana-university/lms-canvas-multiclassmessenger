cf10c1ca1eef48d45b02317c92eb000a563f4831
chmaurer <chmaurer@iu.edu>
2021-11-02 12:05:36 -0400

    LMSA-7792 - move the configurable path and filename out of env.properties and in to application.yml

--- src/test/java/edu/iu/uits/lms/multiclassmessenger/AppLaunchSecurityTest.java
+++ src/test/java/edu/iu/uits/lms/multiclassmessenger/AppLaunchSecurityTest.java
@@ -53,7 +53,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -62,7 +61,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(AnnouncementController.class)
 @Import({ToolConfig.class, CanvasClientTestConfig.class, LtiClientTestConfig.class})
-@ActiveProfiles("none")
 public class AppLaunchSecurityTest {
 
    @Autowired
